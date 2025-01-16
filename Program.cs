2699447d366c9f8941512d232dee2cb5e654b98f
fedotovaaaaa <ivankalmykov836@gmail.com>
2025-01-16 11:52:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student ST = new Student();
+            ST.FIO = "Иванов";
+            ST.Ocenka1 = 3;
+            ST.Ocenka2 = 5;
+            ST.show();
+            Console.WriteLine($"Среднея оценка студента {ST.SredOcenka()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musik
+{
+    internal class Student
+    {
+        public string FIO { get; set; }
+        public int Ocenka1 { get; set; }
+        public int Ocenka2 { get; set; }
+        public Student()
+        {
+            FIO = "";
+            Ocenka1 = 0;
+            Ocenka2 = 0;
+        }
+        public void show()
+        {
+            Console.WriteLine($"Студента зовут: {FIO}, Имеет оценку {Ocenka1} и оценку {Ocenka2}");
+        }
+        public float SredOcenka ()
+        {
+
+            float SO = (Ocenka1 + Ocenka2) / 2;
+            return SO;
+        }
+    }
+}
